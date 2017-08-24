5f526ec4415b05c8f8dc6bfb44fdb85c5924c6c5
Will Whitney <wfwhitney@gmail.com>
2017-08-24 16:13:18 -0400

    add some alternative loss functions to simple VAE

--- vae.py
+++ vae.py
@@ -63,7 +63,8 @@ class VAE(nn.Module):
         self.fc21 = nn.Linear(400, 20)
         self.fc22 = nn.Linear(400, 20)
         self.fc3 = nn.Linear(20, 400)
-        self.fc4 = nn.Linear(400, 784)
+        self.fc41 = nn.Linear(400, 784)
+        self.fc42 = nn.Linear(400, 784)
 
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
@@ -83,12 +84,13 @@ class VAE(nn.Module):
 
     def decode(self, z):
         h3 = self.relu(self.fc3(z))
-        return self.sigmoid(self.fc4(h3))
+        return self.sigmoid(self.fc41(h3)), self.fc42(h3)
 
     def forward(self, x):
         mu, logvar = self.encode(x.view(-1, 784))
         z = self.reparametrize(mu, logvar)
-        return self.decode(z), mu, logvar
+        xhat = self.decode(z)
+        return xhat, mu, logvar
 
 class GaussianKLD(nn.Module):
     def forward(self, q, p):
@@ -107,17 +109,38 @@ class GaussianKLD(nn.Module):
         b = torch.sum(torch.exp(log_sigma_q - log_sigma_p), 1, keepdim=True)
 
         mu_diff = mu_p - mu_q
-        c = torch.sum(torch.pow(mu_diff, 2) / exp(log_sigma_p), 1, keepdim=True)
+        c = torch.sum(
+            torch.pow(mu_diff, 2) / torch.exp(log_sigma_p), 1, keepdim=True)
 
         D = mu_q.size(1)
         divergences = torch.mul(a + b + c - D, 0.5)
         return divergences.mean()
+
+class GaussianLL(nn.Module):
+    def forward(self, p, target):
+        # print(p[0].size())
+        # print(target.size())
+        (mu, log_sigma) = p
+        mu = batch_flatten(mu)
+        log_sigma = batch_flatten(log_sigma)
+        target = batch_flatten(target)
+
+        # sigma = Variable(torch.ones(sigma.size()).type_as(sigma.data) / 10)
+
+        a = torch.sum(log_sigma, 1, keepdim=True)
+        diff = (target - mu)
+        b = torch.sum(
+            torch.pow(diff, 2) / torch.exp(log_sigma), 1, keepdim=True)
+        c = mu.size(1) * math.log(2*math.pi)
+        log_likelihoods = -0.5 * (a + b + c)
+        return log_likelihoods.mean()
 
 model = VAE()
 if args.cuda:
     model.cuda()
 
 gaussianKL = GaussianKLD()
+gaussianLL = GaussianLL()
 reconstruction_function = nn.BCELoss()
 reconstruction_function.size_average = False
 
@@ -133,10 +156,27 @@ def loss_function(recon_x, x, mu, logvar):
 
     return BCE + KLD, KLD, BCE
 
-# def gaussian_loss_function(recon_x, x, mu, logvar):
-#     output_KLD = gaussianKL(recon_x, )
+def gaussianKL_loss_function(xhat, x, mu, logvar):
+    # -2.3 is log(0.1)
+    # pdb.set_trace()
+    target_logvar = Variable(torch.Tensor(x.size()).fill_(-2.3)).type_as(x)
+    output_KLD = gaussianKL(xhat, (x, target_logvar))
 
+    KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
+    KLD = torch.sum(KLD_element).mul_(-0.5)
 
+    return output_KLD + KLD, KLD, output_KLD
+
+def gaussianLL_loss_function(xhat, x, mu, logvar):
+    # pdb.set_trace()
+    output_NLL = - gaussianLL(xhat, x)
+
+    KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
+    KLD = torch.sum(KLD_element).mul_(-0.5)
+
+    return output_NLL + KLD, KLD, output_NLL
+
+loss_function = gaussianLL_loss_function
 
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
@@ -201,8 +241,10 @@ for epoch in range(1, args.epochs + 1):
     logging.debug(format_string.format(*log_values))
 
     z = Variable(torch.randn(args.batch_size, 20), volatile=True)
+    if args.cuda:
+        z = z.cuda()
     # pdb.set_trace()
-    generations = model.decode(z)
+    generations = model.decode(z)[0]
     generations = generations.resize(128, 28, 28)
 
     save_tensors_image("{}/generations{}.png".format(args.save, epoch),
