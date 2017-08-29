fix generation from prior with logvar2; pass variance to inference network

models.py
@@ -238,9 +238,9 @@ class IndependentModel(nn.Module):
                 # give it the sample from z_{t-1}
                 # inferred_z_post = self.inference(reshaped_sequence[t+1], z_sample)
 
-                # give it the mean of the prior p(z_t | z_{t-1})
+                # give it the mean and logvar2 of the prior p(z_t | z_{t-1})
                 inferred_z_post = self.inference(reshaped_sequence[t+1],
-                                                 cat_prior[0])
+                                                 cat_prior)
                 # z_var_mean += cat_prior[1].mean().data[0]
                 # z_var_min = min(z_var_min, cat_prior[1].data.min())
                 # z_var_max = max(z_var_max, cat_prior[1].data.max())

tinydcgan_modules.py
@@ -131,7 +131,8 @@ class TinyDCGANInference(nn.Module):
         # self.conv1 = nn.Conv2d(input_dims[0], 32, 3)
 
         self.input_lin = nn.Linear(prod(self.out_dims[-1]), hidden_dim)
-        self.joint_lin = nn.Linear(hidden_dim * 2, hidden_dim)
+        self.joint_lin = nn.Linear(hidden_dim * 3, hidden_dim)
+        self.merged_lin = nn.Linear(hidden_dim, hidden_dim)
         # self.layers = nn.ModuleList([nn.Linear(hidden_dim, hidden_dim)
         #                              for _ in range(2)])
 
@@ -140,7 +141,7 @@ class TinyDCGANInference(nn.Module):
         self.lin_sigma = nn.Linear(hidden_dim, self.hidden_dim)
 
 
-    def forward(self, x_t, z_prev):
+    def forward(self, x_t, prior):
         current = x_t
         for conv in self.convs:
             current = conv(current)
@@ -149,8 +150,9 @@ class TinyDCGANInference(nn.Module):
         current = self.input_lin(current)
         current = activation(current)
 
-        joined = torch.cat([current, z_prev], 1)
+        joined = torch.cat([current, *prior], 1)
         new_hidden = activation(self.joint_lin(joined))
+        new_hidden = activation(self.merged_lin(joined))
 
         # mu = 10 * F.tanh(self.lin_mu(new_hidden) / 10)
         mu = self.lin_mu(new_hidden)

vae.py
@@ -587,7 +587,7 @@ class VAE(nn.Module):
 #         log_likelihoods = -0.5 * (a + b + c)
 #         return log_likelihoods.mean()
 
-from loss_modules import LogSquaredGaussianKLD, GaussianLL
+from loss_modules import LogSquaredGaussianKLD, GaussianLL, LogSquaredGaussianLL
 
 model = VAE()
 if args.cuda:
@@ -599,6 +599,7 @@ if args.cuda:
 logsquaredgaussianKL = LogSquaredGaussianKLD()
 
 gaussianLL = GaussianLL()
+logsquaredLL = LogSquaredGaussianLL()
 # othergaussianLL = OtherGaussianLL()
 # originalgaussianLL = OriginalGaussianLL()
 # logsquaredLL = LogSquaredGaussianLL()
@@ -646,11 +647,12 @@ def gaussianLL_loss_function(xhat, x, mu, logvar):
                       requires_grad=False).type_as(x)
     # pdb.set_trace()
     log_squared_outvar = Variable(
-        torch.Tensor(xhat[0].size()).fill_(math.log(0.5**2)), 
+        torch.Tensor(xhat[0].size()).fill_(math.log(0.1**2)), 
         requires_grad=False).type_as(x)
 
+    output_NLL = - logsquaredLL(xhat, x)
     # output_NLL = - logsquaredLL((xhat[0], log_squared_outvar), x)
-    output_NLL = - gaussianLL((xhat[0], outvar), x)
+    # output_NLL = - gaussianLL((xhat[0], outvar), x)
 
     # output_NLL = - gaussianLL((xhat[0], log_outvar), x)
     # output_NLL = - gaussianLL(xhat, x)