52b319402e66e403ddf0c5343aefcce4af82c3c8
Will Whitney <wfwhitney@gmail.com>
2017-10-06 16:49:10 -0400

    new style of covariance

--- covariance.py
+++ covariance.py
@@ -14,125 +14,56 @@ from PIL import Image
 import progressbar
 import logging
 
-from util import *
-
-from modules import *
-from models import *
-from env import *
-from params import *
-from generations import *
-from atari_dataset import AtariData
-from video_dataset import *
-from setup import normalize_data
-
+import matplotlib.pyplot as plt
+import seaborn
+import pandas as pd
 import sklearn.covariance
 
+from util import ensure_path_exists
+
+
 @ensure_path_exists
-def construct_covariance(savedir, model, loader, n, label=""):
-    data = []
-    for i, batch in enumerate(loader):
-        data.append(batch)
-        if i * loader.batch_size > n:
-            break
+def construct_covariance(savedir, list_of_lists_of_latents):
+    latent_dim = list_of_lists_of_latents[0][0].size()[0]
+    n_latents = len(list_of_lists_of_latents[0])
 
-    KL = GaussianKLD().type(dtype)
-    LL = GaussianLL().type(dtype)
+    list_of_samples = [torch.cat(sample, 0)
+                       for sample in list_of_lists_of_latents]
+    tensor_of_samples = torch.stack(list_of_samples, 0)
+    z_dim = tensor_of_samples.size(1)
+    data = tensor_of_samples.cpu().numpy()
 
-    zs = []
-
-    for i in range(len(data)):
-        x = normalize_data(opt, dtype, data[i])
-
-        for a in x:
-            a.volatile = True
-
-        # inferred_z_post = model.inference(x[0], model.z1_prior)
-        cat_prior = model.z1_prior
-
-        seq_zs = []
-        for t in range(len(x)):
-            inferred_z_post = model.inference(x[t], cat_prior)
-            seq_zs.append(inferred_z_post[0])
-            z_sample = sample(inferred_z_post)
-            # gen_dist = model.generator(z_sample)
-
-            if t < len(x) - 1:
-                cat_prior = model.predict_latent(z_sample)
-                
-        zs.append(seq_zs)
-
-    paired_zs = []
-    for seq_zs in zs:
-        t = random.randint(0, len(seq_zs) - 2)
-        # for t in range(len(seq_zs) - 1):
-        z = seq_zs[t]
-        z_n = seq_zs[t+1]
-        for first, second in zip(z, z_n):
-            catted = torch.cat([first, second], 0)
-            paired_zs.append(catted)
-    zs = None
-
-    paired_zs = [z.data for z in paired_zs]
-    paired_zs = torch.stack(paired_zs, 1)
-    paired_zs = paired_zs.cpu()
-    paired_zs.transpose_(0, 1)
-    paired_zs = paired_zs.numpy()
-
-    z_dim = model.n_latents * model.hidden_dim
-
-    import matplotlib.pyplot as plt
-    import seaborn
-    import pandas as pd
-
-    df = pd.DataFrame(paired_zs)
+    df = pd.DataFrame(data)
     corr_df = df.corr()
-    corr_np = np.array(corr_df)
-
-    # try to free things up?
-    paired_zs = None
+    corr = np.array(corr_df)
 
     fig = plt.figure()
-    corr = corr_np[:z_dim, :z_dim]
     hm = seaborn.heatmap(corr,
-                         xticklabels=model.hidden_dim,
-                         yticklabels=model.hidden_dim)
+                         xticklabels=latent_dim,
+                         yticklabels=latent_dim)
 
-    for i in range(1, model.n_latents):
-        location = i * model.hidden_dim
-        plt.plot([0, z_dim], [location, location], color='black', linewidth=0.5)
-        plt.plot([location, location], [0, z_dim], color='black', linewidth=0.5)
-    name = "{}/autocorr_{}.pdf".format(savedir, label)
-    plt.savefig(name)
-    plt.close(fig)
-
-
-    fig = plt.figure()
-    corr = corr_np[:z_dim, z_dim:]
-    hm = seaborn.heatmap(corr,
-                         xticklabels=model.hidden_dim,
-                         yticklabels=model.hidden_dim)
-
-    for i in range(1, model.n_latents):
-        location = i * model.hidden_dim
-        plt.plot([0, z_dim], [location, location], color='black', linewidth=0.5)
-        plt.plot([location, location], [0, z_dim], color='black', linewidth=0.5)
-    # plt.savefig('results/' + name + '/corr.pdf')
+    for i in range(1, n_latents):
+        location = i * latent_dim
+        plt.plot([0, z_dim], [location, location],
+                 color='black', linewidth=0.5)
+        plt.plot([location, location], [0, z_dim],
+                 color='black', linewidth=0.5)
     name = "{}/corr_{}.pdf".format(savedir, label)
     plt.savefig(name)
     plt.close(fig)
 
     fig = plt.figure()
     cov = np.array(df.cov())
-    # cov = sklearn.covariance.empirical_covariance(paired_zs)
     hm = seaborn.heatmap(cov[:z_dim, z_dim:],
-                         xticklabels=model.hidden_dim,
-                         yticklabels=model.hidden_dim)
+                         xticklabels=latent_dim,
+                         yticklabels=latent_dim)
 
-    for i in range(1, model.n_latents):
-        location = i * model.hidden_dim
-        plt.plot([0, z_dim], [location, location], color='black', linewidth=0.5)
-        plt.plot([location, location], [0, z_dim], color='black', linewidth=0.5)
-    # plt.savefig('results/' + name + '/cov.pdf')
+    for i in range(1, n_latents):
+        location = i * latent_dim
+        plt.plot([0, z_dim], [location, location],
+                 color='black', linewidth=0.5)
+        plt.plot([location, location], [0, z_dim],
+                 color='black', linewidth=0.5)
     name = "{}/cov_{}.pdf".format(savedir, label)
     plt.savefig(name)
     plt.close(fig)
@@ -150,13 +81,13 @@ if __name__ == "__main__":
         chunk_length=20, train_frac=0.5)
     # dataset = VideoData('/speedy/data/urban/tuesday_4fps.MP4', 5, framerate=2)
     train_loader = DataLoader(train_data,
-                        num_workers=0,
-                        batch_size=opt.batch_size,
-                        shuffle=True)
+                              num_workers=0,
+                              batch_size=opt.batch_size,
+                              shuffle=True)
     test_loader = DataLoader(test_data,
-                        num_workers=0,
-                        batch_size=opt.batch_size,
-                        shuffle=True)
+                             num_workers=0,
+                             batch_size=opt.batch_size,
+                             shuffle=True)
 
     checkpoint = torch.load('results/' + opt.load + '/model.t7')
     model = checkpoint['model'].type(dtype)
